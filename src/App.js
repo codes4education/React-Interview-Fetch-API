76c44cb3a59d6f8c021f068a2d7055006027ab9b
Bhaskar Gupta <codeforeducations@gmail.com>
2024-08-24 17:08:12 +0530

    Fetch API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try{
+        setLoading(true);
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+        if(!response.ok){
+          throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+        setData(result);
+      }catch(error){
+        setError(error.message);
+      } finally{
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  },[]);
+
+  console.log("data:", data);
+
+  if(loading){
+    return <p className="loading">Loading...</p>;
+  }
+
+  if(error){
+    return <p className="error">Error: {error}</p>
+  }
+
+
+
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
